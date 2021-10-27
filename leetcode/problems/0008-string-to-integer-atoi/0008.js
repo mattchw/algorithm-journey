04c1b33b16d697e098bc9ec136b8be5df813ca54
Matthew Wong <matthew.chohin@gmail.com>
2021-10-28 00:28:01 +0800

    feat: add 0008

---
+++ leetcode/problems/0008-string-to-integer-atoi/0008.js
@@ -0,0 +1,27 @@
+/**
+ * Runtime: 100 ms
+ * Memory Usage: 41.4 MB
+ */
+ var myAtoi = function(s) {
+  let i = 0;
+  let sign = 1;
+  let res = 0;
+  let INT_MAX = Math.pow(2, 31) - 1;
+  let INT_MIN = - INT_MAX - 1;
+  
+  while (s[i] === ' ') i++;
+  
+  if (s[i] === '+' || s[i] === '-') {
+      sign = s[i] === '+' ? 1 : -1;
+      i++;
+  }
+  
+  while (s[i] >= '0' && s[i] <= '9') {
+      res = (res * 10) + (s[i] - 0);
+      if (sign === 1 && res > INT_MAX) return INT_MAX;
+      if (sign === -1 && res > INT_MAX + 1) return INT_MIN;
+      i++;
+  }
+  
+  return res * sign;
+};
