cd7fc8b0dc6af28010ca34c164a70341fcce6ef9
Matthew Wong <matthew.chohin@gmail.com>
2022-04-07 23:09:06 +0800

    feat: add reverse string except first and last

---
+++ interview/reverse-string/reverse-string-except-first-and-last.js
@@ -0,0 +1,49 @@
+/**
+ * input:
+ * 6
+ * Hello
+ * Hello World
+ * Hello My World
+ * Hello is my world
+ * Hello is my little world
+ * Hello this is my little world
+ * 
+ * output:
+ * Hello
+ * Hello World
+ * Hello My World
+ * Hello my is world
+ * Hello little my is world
+ * Hello little my is this world
+ */
+
+function reverseStringExceptFirstAndLast(input) {
+  let inputs = input.split('\n');
+
+  // remove the first line
+  inputs.shift();
+
+  for (let i = 0; i < inputs.length; i++) {
+    let words = inputs[i].split(' ');
+    if (words.length > 3) {
+      let firstWord = words[0];
+      let lastWord = words[words.length - 1];
+      let middleWords = words.slice(1, words.length - 1);
+      middleWords.reverse();
+      inputs[i] = `${firstWord} ${middleWords.join(' ')} ${lastWord}`;
+      console.log(inputs[i])
+    } else {
+      console.log(inputs[i])
+    }
+  }
+}
+
+let input = `6
+Hello
+Hello World
+Hello My World
+Hello is my world
+Hello is my little world
+Hello this is my little world`;
+
+reverseStringExceptFirstAndLast(input);
