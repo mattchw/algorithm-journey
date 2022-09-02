f36ad714469a51358b1396ab97dabdbf02a44119
Matthew Wong <matthew.chohin@gmail.com>
2022-09-02 09:38:23 +0800

    feat(array): add string reversal

---
+++ practices/array/string/string-reversal.py
@@ -0,0 +1,14 @@
+"""
+Given a string, return a new string with the reversed order of characters.
+
+Examples:
+
+reverse('apple') === 'leppa'
+reverse('hello') === 'olleh'
+reverse('Greetings!') === '!sgniteerG'
+
+"""
+
+
+def reverse(string):
+    return string[::-1]
