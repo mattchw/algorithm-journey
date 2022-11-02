65721f185b90bce237f3718fd7f38cb01040f196
Matthew Wong <matthew.chohin@gmail.com>
2022-11-03 00:14:37 +0800

    feat: add 0128 py

---
+++ leetcode/problems/0128-longest-consecutive-sequence/0128.py
@@ -0,0 +1,10 @@
+def longestConsecutiveSequence(nums):
+    numSet = set(nums)
+    longest = 0
+    for num in numSet:
+        if num - 1 not in numSet:
+            length = 0
+            while num + length in numSet:
+                length += 1
+            longest = max(longest, length)
+    return longest
