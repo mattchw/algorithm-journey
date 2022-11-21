7fd6b678a4535e50bfda9930dfd9b544baabec8b
Matthew Wong <matthew.chohin@gmail.com>
2022-11-22 00:02:55 +0800

    feat: add linked list js

---
+++ practices/linked-list/linked-list.js
@@ -0,0 +1,132 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+  size() {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+    return counter;
+  }
+  getFirst() {
+    return this.head;
+  }
+  getLast() {
+    if (this.head === null) {
+      return null;
+    }
+
+    let node = this.head;
+
+    while (node) {
+      if (node.next === null) {
+        return node;
+      }
+      node = node.next;
+    }
+  }
+  clear() {
+    this.head = null;
+  }
+  removeFirst() {
+    if (this.head === null) {
+      return;
+    }
+    this.head = this.head.next;
+  }
+  removeLast() {
+    if (this.head === null) {
+      return;
+    }
+
+    if (this.head.next === null) {
+      this.head = null;
+      return;
+    }
+
+    let previous = this.head;
+    let node = this.head.next;
+
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+  }
+  insertLast(data) {
+    const last = this.getLast();
+
+    if (last) {
+      last.next = new Node(data);
+    } else {
+      this.head = new Node(data);
+    }
+  }
+  getAt(index) {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    return null;
+  }
+  removeAt(index) {
+    if (this.head === null) {
+      return;
+    }
+
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+
+    const previous = this.getAt(index - 1);
+    if (!previous || !previous.next) {
+      return;
+    }
+    previous.next = previous.next.next;
+  }
+  insertAt(data, index) {
+    if (this.head === null) {
+      this.head = new Node(data);
+      return;
+    }
+
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+
+    const previous = this.getAt(index - 1) || this.getLast();
+    previous.next = new Node(data, previous.next);
+  }
+  forEach(fn) {
+    let node = this.head;
+    let index = 0;
+
+    while (node) {
+      fn(node, index);
+      node = node.next;
+      index++;
+    }
+  }
+}
