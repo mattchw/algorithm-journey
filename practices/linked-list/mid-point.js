10fe35ac28590ef9e06487ee4469ed5dc42c8a2c
Matthew Wong <matthew.chohin@gmail.com>
2022-12-07 22:53:45 +0800

    feat(linked-list): add midpoint

---
+++ practices/linked-list/mid-point.js
@@ -0,0 +1,30 @@
+/**
+ * Return the 'middle' node of a linked list.
+ * 
+ * If the list has an even number of elements return the node at the end of the first half of the list.
+ * 
+ * Do not use a counter variable
+ * 
+ * Do not retrieve the size of the list
+ * 
+ * Only iterate through the list one time
+ * 
+ * Example:
+ * const l = new LinkedList();
+ * l.insertLast('a')
+ * l.insertLast('b')
+ * l.insertLast('c')
+ * midpoint(l); // returns { data: 'b' }
+ */
+
+function midpoint(list) {
+  const slow = list.getFirst();
+  const fast = list.getFirst();
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+}
