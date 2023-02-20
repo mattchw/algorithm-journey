bf710c091d0798a8ad6fabaad3de59d276c435fa
Matthew Wong <matthew.chohin@gmail.com>
2023-02-20 18:23:53 +0000

    feat(sort): add selection sort js

---
+++ practices/sort/selection-sort.js
@@ -0,0 +1,18 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIdx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIdx]) {
+        minIdx = j;
+      }
+    }
+
+    if (i !== minIdx) {
+      // swap
+      const tmp = arr[i];
+      arr[i] = arr[minIdx];
+      arr[minIdx] = tmp;
+    }
+  }
+  return arr;
+}
