804599bd9ba252be1acaf98ab71c13806474d685
Matthew Wong <matthew.chohin@gmail.com>
2023-02-20 11:57:37 +0000

    feat(tree): add binary search tree js

---
+++ practices/tree/binary-search-tree.js
@@ -0,0 +1,47 @@
+/**
+ * Implement the Node class to create a binary search tree.  The constructor should initialize values 'data', 'left', and 'right'.
+ * 
+ * Implement the 'insert' method for the Node class.  Insert should accept an argument 'data', then create an insert a new node at the appropriate location in the tree.
+ * 
+ * Implement the 'contains' method for the Node class.  Contains should accept a 'data' argument and return the Node in the tree with the same value.
+ */
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    if (this.data > data) {
+      if (this.left) {
+        this.left.insert(data);
+      } else {
+        this.left = new Node(data);
+      }
+    } else {
+      if (this.right) {
+        this.right.insert(data);
+      } else {
+        this.right = new Node(data);
+      }
+    }
+  }
+
+  contains(data) {
+    if (this.data === data) {
+      return this;
+    }
+
+    if (this.data > data && this.left) {
+      return this.left.contains(data);
+    }
+
+    if (this.data < data && this.right) {
+      return this.right.contains(data);
+    }
+
+    return null;
+  }
+}
