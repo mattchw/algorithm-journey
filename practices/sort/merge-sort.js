9e51b91a504fc4b28cbb41190a5b96d229655e26
Matthew Wong <matthew.chohin@gmail.com>
2023-02-20 18:24:13 +0000

    feat(sort): add merge sort js

---
+++ practices/sort/merge-sort.js
@@ -0,0 +1,25 @@
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+  // split
+  const mid = math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const result = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  return [...result, ...left, ...right];
+}
