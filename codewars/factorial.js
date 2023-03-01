3371e691fec42364290cc95f0f1fee16fd31561e
Matthew Wong <matthew.chohin@gmail.com>
2023-03-01 16:46:41 +0000

    fi(codewars): factorial of big numbers js

--- codewars/factorial.js
+++ codewars/factorial.js
@@ -4,26 +4,12 @@
 function factorial(n) {
   let result = [1];
   for (let i = 2; i <= n; i++) {
-    result = multiply(result, i);
-  }
-  return result.join('');
-}
-
-function multiply(a, b) {
-  let result = [];
-  let carry = 0;
-  for (let i = a.length - 1; i >= 0; i--) {
-    let tmp = a[i] * b + carry;
-    if (tmp >= 10) {
-      carry = Math.floor(tmp / 10);
-      tmp = tmp % 10;
-    } else {
-      carry = 0;
+    let tmp = 0;
+    for (let j = 0; j < result.length || tmp > 0; j++) {
+      let product = (result[j] || 0) * i + tmp;
+      result[j] = product % 10;
+      tmp = Math.floor(product / 10);
     }
-    result.unshift(tmp);
   }
-  if (carry > 0) {
-    result.unshift(carry);
-  }
-  return result;
+  return result.reverse().join('');
 }
