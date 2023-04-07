b10d68b404d7273f67c93130c75db5a9866fad29
Matthew Wong <matthew.chohin@gmail.com>
2023-04-07 12:58:34 +0100

    feat(leetcode): add 0125 js

---
+++ leetcode/problems/0125-valid-palidrome/0125.js
@@ -0,0 +1,10 @@
+/**
+ * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+ * 
+ * Given a string s, return true if it is a palindrome, or false otherwise.
+ */
+
+var isPalindrome = function (s) {
+  const str = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return str === str.split('').reverse().join('');
+};
