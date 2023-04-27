8948d540e2c6b8a34587b110d9a17a295e0d3733
Matthew Wong <matthew.chohin@gmail.com>
2023-04-27 14:07:04 +0100

    feat(leetcode): add backtracking solution in 0022 js

--- leetcode/problems/0022-generate-parentheses/0022.js
+++ leetcode/problems/0022-generate-parentheses/0022.js
@@ -2,7 +2,7 @@
  * Runtime: 76 ms
  * Memory Usage: 40.2 MB
  */
-var generateParenthesis = function(n) {
+var generateParenthesis = function (n) {
   let res = [];
   if (n == 0) {
     return res;
@@ -11,15 +11,43 @@ var generateParenthesis = function(n) {
   return res;
 };
 
-var findGenerateParenthesis = function(leftIndex, rightIndex, str, result) {
+var findGenerateParenthesis = function (leftIndex, rightIndex, str, result) {
   if (leftIndex == 0 && rightIndex == 0) {
     result.push(str);
     return;
   }
   if (leftIndex > 0) {
-    findGenerateParenthesis(leftIndex-1, rightIndex, str+"(", result);
+    findGenerateParenthesis(leftIndex - 1, rightIndex, str + "(", result);
   }
   if (rightIndex > 0 && leftIndex < rightIndex) {
-    findGenerateParenthesis(leftIndex, rightIndex-1, str+")", result);
+    findGenerateParenthesis(leftIndex, rightIndex - 1, str + ")", result);
   }
+}
+
+// using backtracking
+var generateParenthesis = function (n) {
+  const stack = [];
+  const res = [];
+
+  const backtrack = (openCount, closeCount) => {
+    if (openCount === n && closeCount === n) {
+      res.push(stack.join(''));
+      return;
+    }
+
+    if (openCount < n) {
+      stack.push('(');
+      backtrack(openCount + 1, closeCount);
+      stack.pop();
+    }
+
+    if (closeCount < openCount) {
+      stack.push(')');
+      backtrack(openCount, closeCount + 1);
+      stack.pop();
+    }
+  }
+
+  backtrack(0, 0);
+  return res;
 }
